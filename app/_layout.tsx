b91d277dacab57d70640e18bbe12000863a42a30
panosjapan7 <panosjapan@gmail.com>
2024-07-13 14:29:39 +0200

    Shows Stack header on login, register

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,8 @@ const Layout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ headerShown: false }} />
-      <Stack.Screen name="register" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: true }} />
+      <Stack.Screen name="register" options={{ headerShown: true }} />
       <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
       <Stack.Screen
         name="reset-password"
