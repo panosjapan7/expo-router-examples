d4e50f28557b545bf30f7bfe11fc62b512a20ce6
panosjapan7 <panosjapan@gmail.com>
2024-07-18 14:21:17 +0200

    Fixes bug in Modal where status bar was black color and therefore not visible

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,48 @@
 // ./app/_layout.tsx
-import { Stack } from "expo-router";
-
+import { Stack, usePathname } from "expo-router";
+import { StatusBar, Platform, useColorScheme } from "react-native";
+import { StatusBar as StatusBarApple } from "expo-status-bar";
 const Layout = () => {
+  const pathname = usePathname();
+  const deviceTheme = useColorScheme();
+
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ headerShown: false, headerTitle: "Home" }}
-      />
-      <Stack.Screen
-        name="login"
-        options={{ headerShown: true, headerTitle: "Login" }}
-      />
-      <Stack.Screen
-        name="register"
-        options={{ headerShown: true, headerTitle: "Register" }}
-      />
-      <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="reset-password"
-        options={{ presentation: "modal", headerTitle: "Reset Password" }}
-      />
-    </Stack>
+    <>
+      {Platform.OS === "android" ? (
+        <StatusBar barStyle={"dark-content"} />
+      ) : null}
+      {Platform.OS === "ios" ? (
+        <StatusBarApple
+          style={
+            deviceTheme === "light" && pathname === "/reset-password"
+              ? "light"
+              : deviceTheme === "light"
+              ? "dark"
+              : "light"
+          }
+        />
+      ) : null}
+
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ headerShown: false, headerTitle: "Home" }}
+        />
+        <Stack.Screen
+          name="login"
+          options={{ headerShown: true, headerTitle: "Login" }}
+        />
+        <Stack.Screen
+          name="register"
+          options={{ headerShown: true, headerTitle: "Register" }}
+        />
+        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="reset-password"
+          options={{ presentation: "modal", headerTitle: "Reset Password" }}
+        />
+      </Stack>
+    </>
   );
 };
 
